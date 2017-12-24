843a52174580034b709924169dcde95278743aea
alrasch <arasch855@gmail.com>
2017-12-24 02:46:27 +0100

    FEATURE - Add method for finding smallest proper divisor.

--- src/alrasch/elementary_ops/Division.java
+++ src/alrasch/elementary_ops/Division.java
@@ -1,5 +1,8 @@
 package alrasch.elementary_ops;
 
+import alrasch.common.exceptions.InvalidArgumentException;
+import alrasch.prime.Prime;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -38,5 +41,32 @@ public class Division {
         }
 
         return divisors.stream().mapToLong(l -> l).toArray();
+    }
+
+    public long getSmallestProperDivisor(long n) throws InvalidArgumentException {
+        if (n == 1) {
+            String message = "Tried to find greatest proper divisor of 1.";
+            String explanation = "1 has only one divisor; itself.";
+            throw new InvalidArgumentException(message, explanation);
+        }
+
+        if (n % 2 == 0) {
+            return 2;
+        }
+
+        for (int i = 3; i <= Math.sqrt(n); i += 2) {
+            if (n % i == 0) {
+                return (long) i;
+            }
+        }
+
+        Prime prime = new Prime();
+        if (prime.isPrime(n)) {
+            String message = "Tried to find greatest proper divisor of a prime.";
+            String explanation = "A prime has only two divisors; 1 and itself. Both are improper.";
+            throw new InvalidArgumentException(message, explanation);
+        }
+
+        return n;
     }
 }
--- tests/alrasch/tests/elementary_ops/DivisionTest.java
+++ tests/alrasch/tests/elementary_ops/DivisionTest.java
@@ -1,8 +1,11 @@
 package alrasch.tests.elementary_ops;
 
+import alrasch.common.exceptions.InvalidArgumentException;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import alrasch.elementary_ops.Division;
 
 public class DivisionTest {
@@ -20,6 +23,23 @@ public class DivisionTest {
 
         for (int i = 0; i < numbers.length; i++) {
             assertArrayEquals(expectations[i], d.getDivisors(numbers[i], false, false));
+        }
+    }
+
+    @Test
+    void testGetSmallestProperDivisor() throws InvalidArgumentException {
+        Division d = new Division();
+
+        long[] numbers = {
+                4L, 6L, 9L, 15L, 35L, 77L, 121L
+        };
+
+        long[] expectations = {
+                2L, 2L, 3L, 3L, 5L, 7L, 11L
+        };
+
+        for (int i = 0; i < numbers.length; i++) {
+            assertEquals(expectations[i], d.getSmallestProperDivisor(numbers[i]));
         }
     }
 }
